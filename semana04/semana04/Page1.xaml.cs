e2cfd8086c980ddd295c789f17b8ca4608ebda34
esHakomo <eber.solano@tecsup.edu.pe>
2023-09-29 15:05:07 -0500

    Nuevas páginas agregadas

---
+++ semana04/semana04/Page1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana04
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentView
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
